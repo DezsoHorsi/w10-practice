fc6d0c29bcfefaae6d2880f5e8d1188a85217363
Dezső Horsi <dezso.horsi@gmail.com>
2023-04-04 16:19:24 +0200

    backend2 has been completed

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,19 @@
+
+
 console.log('loaded')
+
+fetch(`http://127.0.0.1:9000/data`)
+.then(response => {
+  console.log(response)
+  if (response.status === 201) {
+    console.log('ok')
+  }
+  return response.json()
+})
+.then(responseJson => {
+  const data = responseJson
+  data.forEach(element => {
+    console.log(element.name)
+    document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`)
+  });
+})
