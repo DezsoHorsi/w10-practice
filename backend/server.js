e76fae87a02a8df16c99e61f84e930f47a30c885
Dezső Horsi <dezso.horsi@gmail.com>
2023-04-05 16:54:19 +0200

    upload data and files practice has been completed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const path = require('path') //importálás, ezzel tudunk a file-rendszerünkben mozogni.
+const fileUpload = require('express-fileupload')
+const path = require('path')
 const fs = require("fs")
 const app = express()
 const port = 9000
+
+app.use(fileUpload())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
@@ -10,16 +13,13 @@ app.get('/', (req, res) => {
 
 app.use('/public', express.static(`${__dirname}/../frontend/public`))
 
-app.get('/data', (req, res) => {
+app.get('/data', (req, res) => { 
   res.sendFile(path.join(`${__dirname}/data/data.json`))
-})
+}) 
 
 app.get('/data/:id', (req, res) => {
-  //console.log(req.params)
 	try {
-		const searchId = parseInt(req.params.id)
-		//console.log(searchId)
-
+		const searchId = parseInt(req.params.id) 
 		if(isNaN(searchId)) {
 			res.status(418).send("NaN")
 		} else {
@@ -49,16 +49,30 @@ app.get('/data/:id', (req, res) => {
 	}
 })
 
+app.post('/upload', function(req, res) {
+  let uploadedFile;
+  let savePath;
+	let imageName;
 
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  uploadedFile = req.files.image;
+	imageName = req.body.name;
+  savePath = `${__dirname}/data/${imageName}.jpg`;
 
+  // Use the mv() method to place the file somewhere on your server
+  uploadedFile.mv(savePath, (err) => {
+    if (err)
+      return res.status(500).send(err);
 
+    res.json(resData); // imageName = "pelda"
+  });
+});
 
-
-
-
-
-
+//1. ez legyen mindig a legutolsó a kódsorban 
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`)
 })
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -2,18 +2,47 @@
 
 console.log('loaded')
 
+
 fetch(`http://127.0.0.1:9000/data`)
-.then(response => {
-  console.log(response)
-  if (response.status === 201) {
-    console.log('ok')
-  }
-  return response.json()
-})
-.then(responseJson => {
-  const data = responseJson
-  data.forEach(element => {
-    console.log(element.name)
-    document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`)
-  });
+
+//ADATOK BEVITELE
+
+const formComponent = () => `
+  <form>
+  <input type="text" name="name" placeholder="image name">
+  <input type="file" name="file">
+  <button>send</button> 
+  </form>
+`
+//formon belül benn a button egy submit button lesz
+
+const rootElement = document.querySelector('#root')
+
+rootElement.insertAdjacentHTML('beforeend', formComponent())
+
+const formElement= document.querySelector('form')
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault()
+  console.log('submit')
+
+  const formData = new FormData()
+  formData.append('name', document.querySelector("input[type='text']").value)
+  formData.append('image', document.querySelector("input[type='file']").files[0])
+  fetch('/upload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(res => {
+    if(res.status === 200) {
+      console.log("success")
+      return res.json() // return "pelda"
+    } else {
+      console.log("ERROR!!!")
+    }
+  })
+  .then(resData => { // resData = "pelda"
+    rootElement.insertAdjacentHTML('beforeend', `<img scr="public/${resData}.jpeg">`)
+  })
+  .catch(error => console.log(error))
 })
